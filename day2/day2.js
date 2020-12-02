69cc36176332b7334b6080baf2fbf5057d6ba0b5
Matthew Piorko <Piorko.m@husky.neu.edu>
2020-12-02 00:35:28 -0500

    Initial commit

---
+++ day2/day2.js
@@ -0,0 +1,45 @@
+var fs = require("fs");
+
+function parseString(unparsedString) {
+  let re = /(\d+)-(\d+) (\w+): (\w+)/;
+
+  return unparsedString.match(re);
+}
+
+function isValid(unparsedString) {
+  let parsedString = parseString(unparsedString)
+
+  let minOccurences = parsedString[1];
+  let maxOccurences = parsedString[2];
+  let letter = parsedString[3];
+  let password = parsedString[4];
+
+  let occurences = (password.replace(new RegExp(`[^${letter}]`, 'g'), '')).length;
+
+  if (occurences < minOccurences || occurences > maxOccurences) return false;
+
+  return true;
+}
+
+function isValidSecondAlgorithm(unparsedString) {
+  let parsedString = parseString(unparsedString);
+
+  let firstPosition = parsedString[1];
+  let secondPosition = parsedString[2];
+  let letter = parsedString[3];
+  let password = parsedString[4];
+
+  let firstMatches = password.charAt(firstPosition - 1) == letter;
+  let secondMatches = password.charAt(secondPosition - 1) == letter;
+
+  if (firstMatches ^ secondMatches) return true;
+
+  return false;
+}
+
+fs.readFile('input.txt', function (err, data) {
+  let input = data.toString().split('\n').map(isValid);
+  console.log(input.reduce((sum, num) => sum + num, 0));
+  let secondInput = data.toString().split('\n').map(isValidSecondAlgorithm);
+  console.log(secondInput.reduce((sum, num) => sum + num, 0));
+});
