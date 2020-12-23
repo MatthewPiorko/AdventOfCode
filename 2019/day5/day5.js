e09363864ec18027e50fecf59181d4d8a4c752f1
Matthew Piorko <Piorko.m@husky.neu.edu>
2020-12-22 19:16:45 -0500

    Added 2019 day7 and day13 part two solutions

--- 2019/day5/day5.js
+++ 2019/day5/day5.js
@@ -6,8 +6,8 @@ function main() {
   let input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString().split('\n');
   let instrs = input[0].trim().split(',');
 
-  console.log(`Part one answer: ${runInstructionsOnList(instrs, ["1"])}`);
-  console.log(`Part two answer: ${runInstructionsOnList(instrs, ["5"])}`);
+  console.log(`Part one answer: ${runInstructionsOnList(instrs.slice(), ["1"])}`);
+  console.log(`Part two answer: ${runInstructionsOnList(instrs.slice(), ["5"])}`);
 }
 
 module.exports = { main };
